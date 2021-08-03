2c3dbf781873b407917c7b2dcd7f7be8d7b23624
TheRayofSeasons <raylawrencehenrisison@gmail.com>
2021-08-03 21:25:03 +0800

    Add: imports in core init file

---
+++ core/__init__.py
@@ -0,0 +1,7 @@
+from .mixins import NameMapperMixin
+from .panthera import Leopard
+from .panthera import Lion
+from .panthera import Jaguar
+from .panthera import Tiger
+from .panthera_core import Gender
+from .panthera_core import Panthera
